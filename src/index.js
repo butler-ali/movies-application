d1383d23b7b866c820352c10279ea0bb28f4e4bd
admin <admin@admins-MacBook-Pro.local>
2019-05-09 15:14:05 -0500

    push with working input for adding movies

--- src/index.js
+++ src/index.js
@@ -1,45 +1,59 @@
-/**
- * es6 modules and imports
- */
 import sayHello from './hello';
 import $ from "jQuery";
 sayHello('World');
 const {getMovies} = require('./api.js');
-/**
- * require style imports
- */
-//ToDo: Make an ajax request to get a listing of all the movies
-
-
-
 
 
 //posting the new movies
-$('#submit').click(function (e) {
-  e.preventDefault();
+function newMovie(){
+  $('#submit').click(function (e) {
+    e.preventDefault();
 
-  //getting input values
-  let movieTitle = $('#title').val();
-  let movieRating = $('#rating').val();
-  const moviePost = {"title": movieTitle, "rating": movieRating};
-  const url = '/api/movies';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(moviePost),
-  };
-  fetch(url, options)
-      .then(
-          $('#movies').html(''),
-          moviesHTML()
-      )
-      .catch(/* handle errors */);
+    //getting input values
+    let movieTitle = $('#title').val();
+    let movieRating = $('#rating').val();
+    const moviePost = {"title": movieTitle, "rating": movieRating};
+    const url = '/api/movies';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(moviePost),
+    };
+    fetch(url, options)
+        .then(
+            $('#movies').html(''),
+            moviesHTML()
+        )
+        .catch(/* handle errors */);
+  });
+}
 
-
-});
-
+function editMovie(){
+  $('#editMovie').click(function (e) {
+    e.preventDefault();
+    console.log(('Hello this is a test for the edit movie'));
+    //getting input values
+    let movieTitle = $('#title').val();
+    let movieRating = $('#rating').val();
+    const moviePost = {"title": movieTitle, "rating": movieRating};
+    const url = '/api/movies';
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(moviePost),
+    };
+    fetch(url, options)
+        .then(
+            $('#movies').html(''),
+            moviesHTML()
+        )
+        .catch(/* handle errors */);
+  });
+}
 
 
 function moviesHTML(){
@@ -50,7 +64,9 @@ function moviesHTML(){
 //For each movie adds the id, title, and rating to the html
 
     movies.forEach(({title, rating, id}) => {
-      let html = `<div>Movie #${id} - ${title} - rating: ${rating} </div>`;
+      let html = `<div id="card"><p>Movie #${id} - ${title} - rating: ${rating}</p> 
+      <br><button type="button" id="editMovie">Edit</button><br><button type="button" id="deleteMovie">Delete</button></div>`;
+
       $('#movies').append(html);
     });
   }).catch((error) => {
@@ -59,6 +75,11 @@ function moviesHTML(){
   });
 
 }
+
+// $("#deleteMovie").click(function () {
+//   $(this).parent.remove(movies.title);
+//   getMovies()
+// });
 
 
 moviesHTML();
